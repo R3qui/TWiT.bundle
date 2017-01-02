71ab4d6b7059cb6eed88d2c22e42c33cb16c37be
Sander <sander@plex.tv>
2017-01-02 23:07:22 +0100

    Do not use short URLs (lots of shows don't have them). Update list with live stream URLs.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,26 +1,20 @@
 SHOWS_XML = "http://static.twit.tv/ShiftKeySoftware/rssFeeds.plist"
 ITUNES_NAMESPACE = {'itunes':'http://www.itunes.com/dtds/podcast-1.0.dtd'}
-COVER_URL = "http://leoville.tv/podcasts/coverart/%s600%s.jpg"
 LIVE_URLS = {
-	'BitGravity 400 Kbps': 'http://twit.live-s.cdn.bitgravity.com/cdn-live-s1/_definst_/twit/live/low/playlist.m3u8',
-	'BitGravity 1 Mbps': 'http://twit.live-s.cdn.bitgravity.com/cdn-live-s1/_definst_/twit/live/high/playlist.m3u8',
-	'Ustream': 'http://iphone-streaming.ustream.tv/ustreamVideo/1524/streams/live/playlist.m3u8',
-	'Flosoft.biz': 'http://hls.twit.tv:1935/flosoft/smil:twitStream.smil/playlist.m3u8'
+	'Flosoft.biz': 'http://hls.twit.tv/flosoft/smil:twitStream.smil/playlist.m3u8'
 }
 
-DATE_FORMAT = "%a, %d %b %Y"
 RE_EP_TITLE = Regex('\s(?=[0-9]+:)')
 RE_EP_NUMBER = Regex('\s([0-9]+)(:|$)')
-RE_EP_URL = Regex('^http://twit\.tv/[^/]+/\d+$')
 
 ####################################################################################################
 def Start():
 
-	ObjectContainer.title1 = "TWiT.TV"
+	ObjectContainer.title1 = 'TWiT.TV'
 	HTTP.CacheTime = CACHE_1HOUR
 
 ####################################################################################################
-@handler('/video/twittv', "TWiT.TV")
+@handler('/video/twittv', 'TWiT.TV')
 def MainMenu():
 
 	oc = ObjectContainer(no_cache=True)
@@ -30,16 +24,16 @@ def MainMenu():
 
 	retired_shows = RetiredShows()
 
-	for feed in XML.ElementFromURL(SHOWS_XML, cacheTime=CACHE_1WEEK).xpath('//array/string'):
-		(title, video_feed, audio_feed, cover, x) = feed.text.split('|',4)
+	for feed in XML.ElementFromURL(SHOWS_XML, cacheTime=CACHE_1DAY).xpath('//array/string'):
+
+		(title, video_feed, audio_feed, cover, junk) = feed.text.split('|', 4)
 
 		if video_feed != '' and title not in retired_shows:
-			show_abbr = video_feed.split('.tv/',1)[1].split('_',1)[0]
 
 			oc.add(DirectoryObject(
-				key = Callback(Show, title=title, url=video_feed, show_abbr=show_abbr, cover=cover, media='video'),
+				key = Callback(Show, title=title, url=video_feed, cover=cover),
 				title = title,
-				thumb = Resource.ContentsOfURLWithFallback(url=[COVER_URL % (show_abbr, 'video'), cover])
+				thumb = Resource.ContentsOfURLWithFallback(url=cover)
 			))
 
 	oc.add(PrefsObject(title='Preferences...'))
@@ -48,11 +42,12 @@ def MainMenu():
 
 ####################################################################################################
 @route('/video/twittv/show', allow_sync=True)
-def Show(title, url, show_abbr, cover, media):
+def Show(title, url, cover):
 
 	oc = ObjectContainer(title2=title)
 
 	for episode in XML.ElementFromURL(url).xpath('//item'):
+
 		if not episode.xpath('./enclosure/@type')[0].startswith('video/'):
 			continue
 
@@ -69,21 +64,6 @@ def Show(title, url, show_abbr, cover, media):
 			continue
 
 		url = episode.xpath('./comments/text()')[0]
-
-		if not RE_EP_URL.search(url):
-			# Not every show has short urls available, fix the ones that don't
-			if show_abbr == 'floss':
-				show_url = 'show/floss-weekly'
-			elif show_abbr == 'htg':
-				show_url = 'show/home-theater-geeks'
-			elif show_abbr == 'ipad':
-				show_url = 'show/ipad-today'
-			elif show_abbr == 'natn':
-				show_url = 'show/the-social-hour'
-			else:
-				show_url = show_abbr
-
-			url = 'http://twit.tv/%s/%s' % (show_url, episode_number)
 
 		try:
 			summary = episode.xpath('./itunes:summary/text()', namespaces=ITUNES_NAMESPACE)[0]
@@ -107,7 +87,7 @@ def Show(title, url, show_abbr, cover, media):
 			summary = summary,
 			originally_available_at = Datetime.ParseDate(date).date(),
 			duration = duration,
-			thumb = Resource.ContentsOfURLWithFallback(url=[COVER_URL % (show_abbr, media), cover])
+			thumb = Resource.ContentsOfURLWithFallback(url=cover)
 		))
 
 	return oc
@@ -115,15 +95,16 @@ def Show(title, url, show_abbr, cover, media):
 ####################################################################################################
 def RetiredShows():
 
-	#page = HTML.ElementFromURL('http://twit.tv/shows', cacheTime=CACHE_1MONTH)
-	#shows = page.xpath('//div[@id="quicktabs_tabpage_3_1"]//a/text()')
 	shows = []
-	shows.extend(['The Giz Wiz'])
+	#shows.extend(['The Giz Wiz'])
 
 	return shows
 
 ####################################################################################################
-def LiveStream(hls_provider='Ustream', include_container=False):
+def LiveStream(hls_provider='Flosoft.biz', include_container=False):
+
+	if hls_provider not in LIVE_URLS:
+		hls_provider = 'Flosoft.biz'
 
 	vco = VideoClipObject(
 		key = Callback(LiveStream, hls_provider=hls_provider, include_container=True),
@@ -132,7 +113,7 @@ def LiveStream(hls_provider='Ustream', include_container=False):
 		thumb = R('icon-twitlive.png'),
 		items = [
 			MediaObject(
-				video_resolution = 'sd',
+				video_resolution = '480',
 				parts = [
 					PartObject(key=HTTPLiveStreamURL(LIVE_URLS[hls_provider]))
 				]
